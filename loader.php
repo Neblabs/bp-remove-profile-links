90765d6054ffe112725da222e89b919a663e4fc2
Ben Jones <icompugeek@gmail.com>
2013-06-21 05:21:09 -0400

    1.2

--- loader.php
+++ loader.php
@@ -1,7 +1,7 @@
 <?php 
 /*
 Plugin Name: BP Remove Profile Links
-Plugin URI: http://www.wordpress.com
+Plugin URI: https://github.com/bphelp/bp-remove-profile-links
 Description: Removes the Auto Created Links Form BP Profiles. 
 No settings just activate it to remove the links.
 Version: 1.2
@@ -9,7 +9,7 @@ Requires at least: WordPress 3.2 / BuddyPress 1.5.
 Tested up to: WordPress 3.6 beta2 / BuddyPress 1.8 beta1
 License: GNU/GPL 2
 Author: @bphelp
-Author URI: http://www.wordpress.com
+Author URI: http://bphelpblog.wordpress.com/
 */
 
 function bp_remove_profile_links_init() {
